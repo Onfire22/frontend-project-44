d1dc09c3c333ee7bc96e986393a0570f2d16e8bf
Максим Некрасов <qweqwevk@gmail.com>
2024-01-27 12:40:24 +0300

    prime fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,14 +3,17 @@ import startEngine from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
-  for (let i = 2; i < Math.abs(num); i += 1) {
-    if (num % i === 0) return false;
+  let result = 0;
+  for (let i = 1; i <= Math.abs(num); i += 1) {
+    if (num % i === 0) {
+      result += 1;
+    }
   }
-  return num !== 1;
+  return result === 2;
 };
 
 const generateRound = () => {
-  const random = getRandomNumber(-20, 20);
+  const random = getRandomNumber(-1, 1);
   const answer = isPrime(random) ? 'yes' : 'no';
   const question = String(random);
   return [answer, question];
