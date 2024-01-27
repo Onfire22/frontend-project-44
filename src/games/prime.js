e6585b44d48d57927d277763cece45a95ee91476
Максим Некрасов <qweqwevk@gmail.com>
2024-01-27 12:57:47 +0300

    prime fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,13 +3,15 @@ import startEngine from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
-  let result = 0;
-  for (let i = 1; i <= Math.abs(num); i += 1) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i < Math.abs(num); i += 1) {
     if (num % i === 0) {
-      result += 1;
+      return false;
     }
   }
-  return result === 2;
+  return num !== 1;
 };
 
 const generateRound = () => {
